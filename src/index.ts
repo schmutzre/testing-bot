a5265abad143677974c3f8a8b59b3c7dcf9335b8
Remo Schmutz <remoschmutz@bluewin.ch>
2023-11-17 13:53:12 +0100

    replicate main bot

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,43 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
+import fs from 'fs';
+import { execSync } from 'child_process';
 
-const text = await Bot.run(getPostText, { dryRun: true });
+interface Paper {
+  title: string;
+  link: string;
+}
 
-console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
+const POSTED_PAPERS_PATH = './postedPapers.json';
+const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
+
+async function main() {
+  const papersData = await getPostText();
+
+  if (papersData && papersData.length > 0) {
+    for (const textData of papersData) {
+      const { title, link, formattedText } = textData;
+
+      if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
+        await Bot.run(() => Promise.resolve(formattedText));
+        
+        postedPapers.papers.push({ title, link });
+        fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
+
+        console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
+      } else {
+        console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
+      }
+    }
+    execSync('git add ' + POSTED_PAPERS_PATH);
+    execSync('git commit -m "Added new posted papers"');
+    execSync('git push origin main'); 
+  } else {
+    console.log(`[${new Date().toISOString()}] No new posts to publish.`);
+  }
+}
+
+main();
+
+
+
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,37 @@
-export default async function getPostText() {
-  // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+import Parser from 'rss-parser';
+import fs from 'fs';
+
+interface Paper {
+  title: string;
+  link: string;
 }
+
+const FEED_URL = 'https://osfpreprints-feed.herokuapp.com/PsyArXiv.rss';
+const POSTED_PAPERS_PATH = './postedPapers.json';
+const postedPapers = JSON.parse(fs.readFileSync(POSTED_PAPERS_PATH, 'utf8'));
+
+const ONE_DAY = 24 * 60 * 60 * 1000;  // One day in milliseconds
+
+export default async function getPostText() {
+  const parser = new Parser();
+  const feed = await parser.parseURL(FEED_URL);
+  const papersToPost = [];
+
+  for (const item of feed.items) {
+    const publicationDate = item.pubDate ? new Date(item.pubDate) : new Date();
+    const currentDate = new Date();
+
+    const isAlreadyPosted = postedPapers.papers.some((paper: Paper) => paper.title === item.title && paper.link === item.link);
+
+    if (!isAlreadyPosted && (currentDate.getTime() - publicationDate.getTime() <= ONE_DAY)) {
+      papersToPost.push({
+        title: item.title,
+        link: item.link,
+        formattedText: `${item.title}: ${item.link}`
+      });
+    }
+  }
+
+  return papersToPost.length > 0 ? papersToPost : null;
+}
+
