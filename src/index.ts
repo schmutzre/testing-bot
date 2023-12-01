9c0fa7c9bbeea07b3e35507f82a14bbf64c60d12
Remo Schmutz <remoschmutz@bluewin.ch>
2023-12-01 12:52:15 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ if (typeof globalThis.fetch === 'undefined') {
 
 BskyAgent.configure({
   fetch: async (httpUri, httpMethod, httpHeaders, httpReqBody) => {
-    // Log request details
     console.log('API Request Details:');
     console.log('URI:', httpUri);
     console.log('Method:', httpMethod);
@@ -22,16 +21,15 @@ BskyAgent.configure({
     const requestOptions = {
       method: httpMethod,
       headers: new Headers(httpHeaders),
-      body: JSON.stringify(httpReqBody), // Stringify the request body
+      body: JSON.stringify(httpReqBody),
     };
 
-    console.log('Final Request Body:', requestOptions.body); // Log the final request body
+    console.log('Final Request Body:', requestOptions.body);
 
     try {
       const response = await fetch(httpUri, requestOptions);
       const responseBody = await response.text();
 
-      // Log response details
       console.log('API Response Details:');
       console.log('Status:', response.status);
       console.log('Headers:', response.headers);
@@ -48,8 +46,6 @@ BskyAgent.configure({
     }
   },
 });
-
-
 
 // Rest of your script
 import Bot from "./lib/bot.js";
@@ -75,45 +71,50 @@ const imageBlobRef = {
 };
 
 async function main() {
-  const papersData = await getPostText();
+  try {
+    const papersData = await getPostText();
 
-  if (papersData && papersData.length > 0) {
-    for (const textData of papersData) {
-      const { title, link, formattedText } = textData;
+    if (papersData && papersData.length > 0) {
+      for (const textData of papersData) {
+        const { title, link, formattedText } = textData;
 
-      const websiteCardEmbed = {
-        "$type": "app.bsky.embed.external",
-        "external": {
-          "uri": link,
-          "title": title,
-          "description": "Your description here",
-          "thumb": imageBlobRef
+        const websiteCardEmbed = {
+          "$type": "app.bsky.embed.external",
+          "external": {
+            "uri": link,
+            "title": title,
+            "description": "Your description here",
+            "thumb": imageBlobRef
+          }
+        };
+
+        const postContent = {
+          "$type": "app.bsky.feed.post",
+          "text": formattedText,
+          "createdAt": new Date().toISOString(),
+          "embed": websiteCardEmbed
+        };
+
+        if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
+          await Bot.run(() => Promise.resolve(postContent));
+          
+          postedPapers.papers.push({ title, link });
+          fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
+
+          console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
+        } else {
+          console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
         }
-      };
-
-      const postContent = {
-        "$type": "app.bsky.feed.post",
-        "text": formattedText,
-        "createdAt": new Date().toISOString(),
-        "embed": websiteCardEmbed
-      };
-
-      if (!postedPapers.papers.some((paper: Paper) => paper.title === title && paper.link === link)) {
-        await Bot.run(() => Promise.resolve(postContent));
-        
-        postedPapers.papers.push({ title, link });
-        fs.writeFileSync(POSTED_PAPERS_PATH, JSON.stringify(postedPapers, null, 2));
-
-        console.log(`[${new Date().toISOString()}] Posted: "${formattedText}"`);
-      } else {
-        console.log(`[${new Date().toISOString()}] Already posted: "${title}"`);
       }
+      execSync('git add ' + POSTED_PAPERS_PATH);
+      execSync('git commit -m "Added new posted papers"');
+      execSync('git push origin main'); 
+    } else {
+      console.log(`[${new Date().toISOString()}] No new posts to publish.`);
     }
-    execSync('git add ' + POSTED_PAPERS_PATH);
-    execSync('git commit -m "Added new posted papers"');
-    execSync('git push origin main'); 
-  } else {
-    console.log(`[${new Date().toISOString()}] No new posts to publish.`);
+  } catch (error) {
+    console.error(`[${new Date().toISOString()}] An error occurred:`, error);
+    process.exit(1);
   }
 }
 
